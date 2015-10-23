b1e2e2ef65cac038234143ee4bb63d712c5f4d65
Georgios A. Tsakalos <tsakalos@admin.grnet.gr>
2015-10-23 13:51:13 +0300

    add commented code to delete non-noc empty places

--- atl/collect_empty_places.py
+++ atl/collect_empty_places.py
@@ -6,8 +6,11 @@ setup_environ(settings)
 from equip.models import Place
 
 places = Place.objects.all()
-with open('unissigned_places_with_noc.txt' , 'w') as f:
+with open('unissigned_places.txt' , 'w') as f:
     for place in places:
         #if place.productcomponent_set.count() == 0:
         if place.productcomponent_set.count() == 0 and place.api_id == None:
             f.write('{} - {}\n'.format(str(place.id), place.name.encode('utf-8')))
+
+            # uncomment to delete empty places that are not from noc api
+            # place.delete()
