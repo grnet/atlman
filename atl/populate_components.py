a2da29f34540323923c3326f597b7a8c7c405859
Georgios A. Tsakalos <tsakalos@admin.grnet.gr>
2015-11-02 18:27:03 +0200

    update populate_components script
    remove mail sending
    add printing for logging

--- atl/populate_components.py
+++ atl/populate_components.py
@@ -3,8 +3,8 @@ import os
 from django.core.management import setup_environ    
 import settings
 setup_environ(settings)
-from django.core.mail import send_mail
 from equip.models import *
+import time
 
 
 products_added = 0
@@ -20,9 +20,12 @@ def populate():
     a = Product.objects.all()
     for b in a:
         b.populate_components()
+    print time.strftime("%d/%m/%y - %H:%M:%S") 
     if products_added > 0:
-        send_mail('%s Products Added'% products_added, '%s new products were added. Please check http://atlman.admin.grnet.gr/equip'% products_added , 'noreply@grnet.gr', ['leopoul@noc.grnet.gr'])
-
+        print '{} products added'.format(str(products_added))
+    else:
+        print 'No products added'
+    print '\n\n'
 
 if __name__ == "__main__":
     populate()
